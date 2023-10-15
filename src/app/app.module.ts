61db58008b31a46951fac0f99c62165e05306dab
Timo Wang <timowang1991@gmail.com>
2023-10-15 21:59:53 +0800

    add to shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HeaderModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', {static: true})
+  nameInputRef: ElementRef;
 
+  @ViewChild('amountInput', {static: true})
+  amountInputRef: ElementRef;
+  
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
